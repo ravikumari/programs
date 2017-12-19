e17fb436d17e94b3c6542479ae00d3e91242f64b
ravikumari <ravikumari@gmail.com>
2017-12-19 21:24:53 +0530

    i am trying

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
     class Simple{  
         public static void main(String args[]){  
-         System.out.println("Hello jenkins world");  
+         System.out.println("Hello simple jenkins world");  
         }  
     }  
