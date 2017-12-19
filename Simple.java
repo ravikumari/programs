8bb33aa0a42e8e9749691e886664f7a257410687
ravikumari <ravikumari@gmail.com>
2017-12-19 21:17:27 +0530

    trying

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
     class Simple{  
         public static void main(String args[]){  
-         System.out.println("Hello jenkins");  
+         System.out.println("Hello jenkins world");  
         }  
     }  
